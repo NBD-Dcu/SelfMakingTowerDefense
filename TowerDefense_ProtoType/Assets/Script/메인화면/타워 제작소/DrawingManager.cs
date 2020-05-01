a0c6d974ca60475600db718c56339db7e46b6048
WalnutTeacher <55396663+WalnutTeacher@users.noreply.github.com>
2020-05-02 03:07:38 +0900

    투사체 제작 기능 추가
    1.투사체 제작 기능 추가
    2.제작소 스크롤 뷰 추가
    3.제작소에서 불러오기를 하고 난 후에 가려진 캔버스에 그림이 그려지는 증상 조치

--- TowerDefense_ProtoType/Assets/Script/메인화면/타워 제작소/DrawingManager.cs
+++ TowerDefense_ProtoType/Assets/Script/메인화면/타워 제작소/DrawingManager.cs
@@ -9,13 +9,19 @@ public class DrawingManager : MonoBehaviour
 {
     public static DrawingManager drawingManager;
     //32픽셀 - 타워
-    float _32DistantUnit = 0.03125f;
     public GameObject _32Canvas;
+    public GameObject _32CanvasCover;
     public GameObject _32TilePrefab;
     Vector2 _32StartPosition = new Vector2(-0.484375f, -0.484375f);
+    float _32DistantUnit = 0.03125f;
     GameObject[,] _32InterActiveTiles = new GameObject[32, 32];
-    //16픽셀 - 투사체
-
+    //8픽셀 - 투사체
+    public GameObject _8Canvas;
+    public GameObject _8CanvasCover;
+    public GameObject _8TilePrefab;
+    Vector2 _8StartPosition = new Vector2(-0.4375f, -0.4375f);//0-0.0625-3*0.125
+    float _8DistantUnit = 0.125f;
+    GameObject[,] _8InterActiveTiles = new GameObject[8, 8];
     //기타등등
     public string towerImagePath;
     public string projectileImagePath;
@@ -34,6 +40,7 @@ public class DrawingManager : MonoBehaviour
     public Image selectedImageView;
     public Text selectedImageNameView;
 
+    public Transform scrollView;
     List<GameObject> interactableButtons = new List<GameObject>();
 
     private void Awake()
@@ -42,20 +49,44 @@ public class DrawingManager : MonoBehaviour
         {
             drawingManager = this;
             _32TilePrefab = Resources.Load<GameObject>(drawingScreenPath + "/" + "32상호작용1픽셀");
+            _8TilePrefab = Resources.Load<GameObject>(drawingScreenPath + "/" + "8상호작용1픽셀");
             CreateInterActiveTiles();
-            ClearCanvas();
+            InitGrid(32, 32);
+            mode = 1;
+            InitGrid(8, 8);
+            mode = 0;
+            GameManager.gameManager.uiCanvas = GameObject.Find("UICanvas");
         }
     }
-
-    void Start()
+    private void Start()
     {
         
     }
-    
-    void Update()
+
+    private void Update()
     {
+        
     }
 
+    //그림 그리기 화면에서 사용
+    public void ChangeMode(int mode)
+    {
+        this.mode = mode;
+        if (mode == 0)
+        {
+            _32Canvas.SetActive(true);
+            _32CanvasCover.SetActive(true);
+            _8Canvas.SetActive(false);
+            _8CanvasCover.SetActive(false);
+        }
+        else if (mode == 1)
+        {
+            _32Canvas.SetActive(false);
+            _32CanvasCover.SetActive(false);
+            _8Canvas.SetActive(true);
+            _8CanvasCover.SetActive(true);
+        }
+    }
     public void ChangePixel(int x, int y, Color color)
     {
         Texture2D tex;
@@ -67,10 +98,11 @@ public class DrawingManager : MonoBehaviour
         }
         else if(mode == 1)
         {
-            
+            tex = _8Canvas.GetComponent<SpriteRenderer>().sprite.texture;
+            tex.SetPixel(x, y, color);
+            tex.Apply();//실제 그림 파일에 픽셀 반영
         }
     }
-    
     public void CreateInterActiveTiles()
     {
         //32픽셀
@@ -80,13 +112,33 @@ public class DrawingManager : MonoBehaviour
             {
                 Vector2 positionValue = new Vector2(_32StartPosition.x + _32DistantUnit * i, _32StartPosition.y + _32DistantUnit * j);
                 GameObject interActiveTile = Instantiate(_32TilePrefab, positionValue, Quaternion.identity);
+
+                interActiveTile.transform.SetParent(_32Canvas.transform);
+
                 _32InterActiveTiles[i, j] = interActiveTile;
-                interActiveTile.GetComponent<_32TileScript>().positionX = i;
-                interActiveTile.GetComponent<_32TileScript>().positionY = j;
+                interActiveTile.GetComponent<TileScript>().positionX = i;
+                interActiveTile.GetComponent<TileScript>().positionY = j;
             }
         }
-    }
 
+        //8픽셀
+        for (int i = 0; i < 8; i++)
+        {
+            for (int j = 0; j < 8; j++)
+            {
+                Vector2 positionValue = new Vector2(_8StartPosition.x + _8DistantUnit * i, _8StartPosition.y + _8DistantUnit * j);
+                GameObject interActiveTile = Instantiate(_8TilePrefab, positionValue, Quaternion.identity);
+
+                interActiveTile.transform.SetParent(_8Canvas.transform);//테스트중
+
+                _8InterActiveTiles[i, j] = interActiveTile;
+                interActiveTile.GetComponent<TileScript>().positionX = i;
+                interActiveTile.GetComponent<TileScript>().positionY = j;
+            }
+        }
+
+
+    }
     void InitGrid(int x, int y)
     {
         for (int i = 0; i < x; i++)
@@ -97,71 +149,86 @@ public class DrawingManager : MonoBehaviour
             }
         }
     }
-
     public void ClearCanvas()
     {
         if (mode == 0)
             InitGrid(32, 32);
+        if (mode == 1)
+            InitGrid(8, 8);
     }
-    
     public void ChangeBrushColor()
     {
         GameObject clikedButton = GameObject.Find(EventSystem.current.currentSelectedGameObject.name);
         currentColor = clikedButton.GetComponent<Image>().color;
     }
-
     public void ChangeBrushToEraser()
     {
         currentColor = new Color(255, 255, 255, 0);
     }
-
     public void SaveImage()
     {
-        if (mode == 0)
+        string folderPath = null;
+        if(mode == 0)
         {
-
-            if (!Directory.Exists(Application.persistentDataPath + towerImagePath))
+            folderPath = towerImagePath;
+        }
+        else if(mode == 1)
+        {
+            folderPath = projectileImagePath;
+        }
+        if (!Directory.Exists(Application.persistentDataPath + folderPath))
+        {
+            Directory.CreateDirectory(Application.persistentDataPath + folderPath);
+        }
+        else
+        {
+            if (InputNameField.text.Length != 0)
             {
-                Directory.CreateDirectory(Application.persistentDataPath + towerImagePath);
-            }
-            else
-            {
-                if (InputNameField.text.Length!=0)
+                bool isExistion = false;
+                DirectoryInfo di = new DirectoryInfo(Application.persistentDataPath + folderPath);
+                foreach (FileInfo f in di.GetFiles())
                 {
-                    bool isExistion = false;
-                    DirectoryInfo di = new DirectoryInfo(Application.persistentDataPath + towerImagePath);
-                    foreach (FileInfo f in di.GetFiles())
+                    string NameInFolder = f.Name.Substring(0, f.Name.Length - 4);
+                    if (f.Extension.ToLower().CompareTo(".png") == 0)
                     {
-                        string NameInFolder = f.Name.Substring(0, f.Name.Length - 4);
-                        if (f.Extension.ToLower().CompareTo(".png") == 0)
-                        {
-                            if (NameInFolder.Equals(InputNameField.text))
-                                isExistion = true;
-                        }
+                        if (NameInFolder.Equals(InputNameField.text))
+                            isExistion = true;
                     }
+                }
 
-                    if (isExistion == false)
+                if (isExistion == false)
+                {
+                    byte[] bytes = null;
+                    if (mode == 0)
                     {
-                        byte[] bytes = _32Canvas.GetComponent<SpriteRenderer>().sprite.texture.EncodeToPNG();
-                        File.WriteAllBytes(Application.persistentDataPath + towerImagePath + "/" + InputNameField.text + ".png", bytes);
+                        bytes = _32Canvas.GetComponent<SpriteRenderer>().sprite.texture.EncodeToPNG();
+                    }
+                    else if(mode == 1)
+                    {
+                        bytes = _8Canvas.GetComponent<SpriteRenderer>().sprite.texture.EncodeToPNG();
+                    }
+                        File.WriteAllBytes(Application.persistentDataPath + folderPath + "/" + InputNameField.text + ".png", bytes);
                         GameManager.gameManager.ShowGuideMessage("파일 저장됨");
-                    }
-                    else
-                    {
-                        GameManager.gameManager.ShowGuideMessage("동일한 이름이 존재합니다");
-                    }
+                    
                 }
                 else
                 {
-                    GameManager.gameManager.ShowGuideMessage("이름을 입력해 주세요");
+                    GameManager.gameManager.ShowGuideMessage("동일한 이름이 존재합니다");
                 }
+            }
+            else
+            {
+                GameManager.gameManager.ShowGuideMessage("이름을 입력해 주세요");
             }
         }
 
     }
-
+    
+    //불러오기 화면에서 사용
     public void LoadLoadScreen()
     {
+        _32Canvas.SetActive(false);
+        _8Canvas.SetActive(false);
         loadScreen.gameObject.SetActive(true);
         drawingScreen.gameObject.SetActive(false);
         for (int i = 0; i < interactableButtons.Count; i++)
@@ -173,7 +240,6 @@ public class DrawingManager : MonoBehaviour
         selectedImageNameView.text = null;
         LoadImageList();
     }
-
     void LoadImageList()
     {
         string FolderPath;
@@ -188,43 +254,19 @@ public class DrawingManager : MonoBehaviour
         List<SpriteWithInformation> imageList = new List<SpriteWithInformation>();
         GameManager.gameManager.LoadImageList(Application.persistentDataPath + FolderPath, imageList);
         GameObject InteractableImageButtonPrefab = Resources.Load<GameObject>("Prefab/DrawingScreen/상호작용 가능한 이미지 버튼"); //상호작용 가능한 이미지 리스트를 나타내는데 사용될 버튼 프리팹
-        int j = 0, k = 0;//나중에 더 나은 구조로 바꾸기
+
         for (int i = 0; i < imageList.Count; i++)
         {
-            //InteractableImageButtonPrefab.GetComponent<Image>().sprite = imageList[i].sprite;
-            GameObject InteractableImageButton = Instantiate(InteractableImageButtonPrefab, loadScreen.transform);//버튼 생성
+            GameObject InteractableImageButton = Instantiate(InteractableImageButtonPrefab, scrollView);//버튼 생성
+            RectTransform rectTransForm = InteractableImageButton.GetComponent<RectTransform>();
+
             InteractableImageButton.GetComponent<Image>().sprite = imageList[i].sprite;
             Debug.Log(imageList[1].spriteName);
             InteractableImageButton.GetComponent<Button>().onClick.AddListener(ClickImage);//버튼에 리스너 부착
             InteractableImageButton.GetComponent<ImageInformation>().filePath = imageList[i].spritePath;
             InteractableImageButton.GetComponent<ImageInformation>().fileName = imageList[i].spriteName;
-            
             interactableButtons.Add(InteractableImageButton);
-            if (InteractableImageButton.transform.parent.GetComponent<RectTransform>().sizeDelta.x / (j * 90) < 1)//해상도에 맞게 고치기
-            {
-                j = 0;
-                k++;
-            }
-            InteractableImageButton.transform.localPosition = new Vector2(-400 + j * 90, 0 - k * 90);//해상도에 맞게 고치기
-            j++;
         }
-    }
-
-
-    public void ChangeCanvasImage()
-    {
-        Sprite sourceImage = selectedImageButton.GetComponent<Image>().sprite;
-        if (mode == 0)
-        {
-            for (int i = 0; i < 32; i++)
-            {
-                for (int j = 0; j < 32; j++)
-                {
-                    ChangePixel(i, j, sourceImage.texture.GetPixel(i, j));
-                }
-            }
-        }
-        TerminateLoadScreen();
     }
     public void ClickImage()
     {
@@ -232,7 +274,6 @@ public class DrawingManager : MonoBehaviour
         selectedImageView.sprite = selectedImageButton.GetComponent<Image>().sprite;
         selectedImageNameView.text = selectedImageButton.GetComponent<ImageInformation>().fileName;
     }
-
     public void DeleteImageFile()
     {
         File.Delete(selectedImageButton.GetComponent<ImageInformation>().filePath);
@@ -247,7 +288,36 @@ public class DrawingManager : MonoBehaviour
     }
     public void TerminateLoadScreen()
     {
+        if (mode == 0)
+            _32Canvas.SetActive(true);
+        else if (mode == 1)
+            _8Canvas.SetActive(true);
         drawingScreen.gameObject.SetActive(true);
         loadScreen.gameObject.SetActive(false);
+    }
+    public void ChangeCanvasImage()
+    {
+        Sprite sourceImage = selectedImageButton.GetComponent<Image>().sprite;
+        if (mode == 0)
+        {
+            for (int i = 0; i < 32; i++)
+            {
+                for (int j = 0; j < 32; j++)
+                {
+                    ChangePixel(i, j, sourceImage.texture.GetPixel(i, j));
+                }
+            }
+        }
+        else if(mode == 1)
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    ChangePixel(i, j, sourceImage.texture.GetPixel(i, j));
+                }
+            }
+        }
+        TerminateLoadScreen();
     }
 }
---
+++ TowerDefense_ProtoType/Assets/Script/메인화면/타워 제작소/TileScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileScript : MonoBehaviour
+{
+    DrawingManager DM = DrawingManager.drawingManager;
+    public int positionX;
+    public int positionY;
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    private void OnMouseDown()
+    {
+        DrawingManager.drawingManager.ChangePixel(positionX, positionY, DM.currentColor);
+    }
+
+    private void OnMouseEnter()
+    {
+        if (Input.GetMouseButton(0))
+            DrawingManager.drawingManager.ChangePixel(positionX, positionY, DM.currentColor);
+    }
+}
